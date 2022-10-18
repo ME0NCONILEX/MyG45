4e20b3a8950c56fce6027edeff4bbced3519a7f5
Valentin <115096856+ME0NCONILEX@users.noreply.github.com>
2022-10-19 00:55:33 +0200

    Delete v2Exer5s.java

--- src/main/java/v2Exer5s.java
+++
@@ -1,9 +0,0 @@
-package week2.strings;
-//String - correction
-public class v2Exer5s {
-    public static void main(String[] args) {
-        String myTxt = ("Java is the worst programming language!");
-        System.out.println("The original stupid sentence is : " + myTxt);
-        System.out.println("But let us to see the truth: " + myTxt.replace("worst", "best"));
-    }
-}
