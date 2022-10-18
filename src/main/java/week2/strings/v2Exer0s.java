8c72b2b499f05ae5ee2ce75ffde735db47524ac8
Valentin <115096856+ME0NCONILEX@users.noreply.github.com>
2022-10-19 00:15:15 +0200

    Create v2Exer0s.java
    Strings - lengtth of

---
+++ src/main/java/week2/strings/v2Exer0s.java
@@ -0,0 +1,8 @@
+package week2.strings;
+//Strings - lengtth of
+public class v2Exer0s {
+    public static void main(String[] args) {
+        String txt = "Java";
+        System.out.println("The length of the 'Java' text string is: " + txt.length());
+    }
+}
