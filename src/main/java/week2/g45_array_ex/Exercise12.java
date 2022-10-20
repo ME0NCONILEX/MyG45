911b26f92c352ae245da0cec2da65feea7d04d10
Valentin <115096856+ME0NCONILEX@users.noreply.github.com>
2022-10-20 16:43:51 +0200

    print the diagonal elements of 2D array

---
+++ src/main/java/week2/g45_array_ex/Exercise12.java
@@ -0,0 +1,41 @@
+package se.lexicon;
+/*
+ * 12.Write a program which will print the diagonal elements of two dimensional array.
+ * Expected output: 1 4 9
+ */
+public class Exercise12 {
+  public static void main(String[] args) {
+    //creates an array
+    int[][] my2DArray = new int[3][3];
+    int column = 1, row = 1;
+    int i = 0;
+    while (i < my2DArray.length) {
+      int j = 0;
+      while (j < my2DArray[i].length) {
+        my2DArray[i][j] = row * column;
+        column = column + 1;
+        j++;
+      }
+      column = 1;
+      row = row + 1;
+      i++;
+    }
+    System.out.println("The 2D array:");
+    i = 0;
+    while (i < my2DArray.length) {
+      int j = 0;
+      while (j < my2DArray[i].length) {
+        System.out.print(" " + my2DArray[i][j] + "\t");
+        j++;
+      }
+      System.out.print("\n");
+      i++;
+    }
+    System.out.println("The diagonal elements in the 2D array:");
+    for (int k = 0; k < my2DArray.length; k++) {
+      System.out.print(my2DArray[k][k] + "\t");
+    }
+  }
+  public static void ex12() {
+  }
+}
