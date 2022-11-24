7b5db2833e130aeef0bbe65b4054921e24adaf52
Valentin <115096856+ME0NCONILEX@users.noreply.github.com>
2022-11-24 21:59:44 +0100

    Update PersonDao.java

--- src/main/java/week6/ToDo/part3/main/java/dao/PersonDao.java
+++ src/main/java/week6/ToDo/part3/main/java/dao/PersonDao.java
@@ -1,4 +1,13 @@
 package part3.main.java.dao;
+import part3.main.java.Model.Person;
+import java.util.List;
+import java.util.Optional;
 
-public class PersonDao {
+public interface PersonDao  {
+    Person create(Person model);
+    Person findById(Integer id);
+    boolean deleteById(Integer id);
+    void update(Person model);
+    List<Person> findAll();
+    Optional<Person> findByUsername(String username);
 }
